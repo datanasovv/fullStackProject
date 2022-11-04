369f07f3fe56355abd2a958ca558fc1328b145a6
Dimitar Atanasov <d.atanasovv96@gmail.com>
2022-11-04 18:56:02 +0200

    handling get request

--- myExpressServer/server.js
+++ myExpressServer/server.js
@@ -2,6 +2,10 @@ const express = require("express");
 
 const app = express();
 
+app.get("/", function (req, res) {
+  res.send("<h1>Hello world</h1>");
+});
+
 app.listen(3000, function () {
   console.log("server started port 3000");
 });
