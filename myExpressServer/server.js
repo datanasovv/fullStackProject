a48ae1341812228ee43ec2d92788ae1feac9f3f9
Dimitar Atanasov <d.atanasovv96@gmail.com>
2022-11-05 23:39:41 +0200

    routes

--- myExpressServer/server.js
+++ myExpressServer/server.js
@@ -6,6 +6,18 @@ app.get("/", function (req, res) {
   res.send("<h1>Hello world</h1>");
 });
 
+app.get("/contact", function (req, res) {
+  res.send("Contact me at: pirate@gmail.com");
+});
+
+app.get("/about", function (req, res) {
+  res.send("My name is Dimitar Atanasov");
+});
+
+app.get("/hobbies", function (req, res) {
+  res.send("<ul><li>Code</li><li>Sports</li></ul>");
+});
+
 app.listen(3000, function () {
   console.log("server started port 3000");
 });
