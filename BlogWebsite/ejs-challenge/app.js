b063af17cea207d26f35b35dd5ce10083c987ba8
Dimitar Atanasov <d.atanasovv96@gmail.com>
2022-11-22 17:52:06 +0200

    display posts on home screen

--- BlogWebsite/ejs-challenge/app.js
+++ BlogWebsite/ejs-challenge/app.js
@@ -18,8 +18,10 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+let posts = [];
+
 app.get("/", function (req, res) {
-  res.render("home", { startingContent: homeStartingContent });
+  res.render("home", { startingContent: homeStartingContent, posts: posts });
 });
 
 app.get("/about", function (req, res) {
@@ -39,6 +41,8 @@ app.post("/compose", function (req, res) {
     title: req.body.postTitle,
     content: req.body.postBody,
   };
+  posts.push(post);
+  res.redirect("/");
 });
 
 app.listen(3000, function () {
